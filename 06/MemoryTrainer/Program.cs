4c14e4b90f19b87e8532b2cba964dfaa249b4cdd
NachinauchiyGaibe <superhoroshiy@mail.ru>
2018-04-04 20:14:43 +0300

    Matematika

--- 06/MemoryTrainer/Program.cs
+++ 06/MemoryTrainer/Program.cs
@@ -14,31 +14,42 @@ namespace MemoryTrainer {
 		static void GuessNumber() {
 			// создаем генератор случайных чисел
 			Random rand = new Random();
-
+            int win = 0;
+            int time = 2000;
 			int maxTarget = 100;
-			while (true) {
-				// генерируем случайное число с помощью генератора
-				int target = rand.Next(maxTarget);
+            while (true)
+            {
+                // генерируем случайное число с помощью генератора
+                int target = rand.Next(maxTarget);
+                {
+                    Console.WriteLine("Запомните число: " + target);
+                    Thread.Sleep(time);
+                    Console.Clear();
 
-				Console.WriteLine("Запомните число: " + target);
-				Thread.Sleep(2000);
-				Console.Clear();
+                    Console.WriteLine("Введите запомненное число");
+                    int guess = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
 
-				Console.WriteLine("Введите запомненное число");
-				int guess = int.Parse(Console.ReadLine());
-				Console.WriteLine();
+                    if (guess == target)
+                    {
+                        Console.WriteLine("Вы угадали!");
+                        maxTarget = maxTarget + 200;
+                        win = win + 1;
+                        if (win >= 10)
+                        {
+                            time = time - 200;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ошиблись ='(");
+                        maxTarget = maxTarget - 100;
+                    }
 
-				if (guess == target) {
-					Console.WriteLine("Вы угадали!");
-					maxTarget = maxTarget + 200;
-				}
-				else {
-					Console.WriteLine("Вы ошиблись ='(");
-					maxTarget = maxTarget - 100;
-				}
-				Thread.Sleep(1000);
-				Console.Clear();
-			}
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
 		}
 	}
 }
